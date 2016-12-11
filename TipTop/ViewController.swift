12add27baa104b573a2019dc5d4f7c2c16616bd4
Rajjwal <rajjwal.rawal@bison.howard.edu>
2016-12-11 14:49:35 -0500

    Initial Commit

---
+++ TipTop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipTop
+//
+//  Created by Rajjwal Rawal on 12/11/16.
+//  Copyright © 2016 Rajjwal Rawal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
